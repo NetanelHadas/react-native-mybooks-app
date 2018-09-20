9ce19aab93e1f6f2b8348b4ad05f71d463a19f6d
DESKTOP-CT28P5M\netan <netanel.hadas@gmail.com>
2018-09-20 18:02:01 +0300

    fixed uppcase function and remove nonNumericChars function

--- src/actions/BookActions.js
+++ src/actions/BookActions.js
@@ -57,14 +57,11 @@ const dateFieldError = (dispatch) => {
   });
 };
 
-const uppercase = (str) => {
-  let array1 = str.split(' ');
-  let newarray1 = [];
-    
-  for(let x = 0; x < array1.length; x++){
-      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
-  }
-  return newarray1.join(' ');
+const uppercase = (text) => {
+   return text = text.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
 }
 
 const removeNonNumericChars = (str) => {
